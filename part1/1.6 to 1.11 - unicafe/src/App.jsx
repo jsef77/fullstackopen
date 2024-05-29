0104a11ede0bc7916fdafc18dcc77ec4151d867c
SEF-3070 <josef.weikert@gmail.com>
2024-05-29 13:57:16 +0100

    1.6: unicafe step 1

--- part1/1.6 to 1.11 - unicafe/src/App.jsx
+++ part1/1.6 to 1.11 - unicafe/src/App.jsx
@@ -1,12 +1,44 @@
 import { useState } from "react";
 
+const Button = ({ text, onClick }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
+const Title = ({ text }) => {
+  return <h1>{text}</h1>;
+};
+
+const Statistic = ({ text, stat }) => {
+  return (
+    <>
+      <p>
+        {text} {stat}
+      </p>
+    </>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  return (
+    <>
+      {/* BUTTONS */}
+      <Title text="give feedback" />
+      <Button text="good" onClick={() => setGood(good + 1)} />
+      <Button text="neutral" onClick={() => setNeutral(neutral + 1)} />
+      <Button text="bad" onClick={() => setBad(bad + 1)} />
+
+      {/* STATS DISPLAY */}
+      <Title text="statistics" />
+      <Statistic text="good" stat={good} />
+      <Statistic text="neutral" stat={neutral} />
+      <Statistic text="bad" stat={bad} />
+    </>
+  );
 };
 
 export default App;
