3577f72d5d1570ace7d487c5682da5e059549b6e
SEF-3070 <josef.weikert@gmail.com>
2024-06-03 16:00:21 +0100

    2.9*: The Phonebook Step 4

--- part2/2.6 to 2.10/src/App.jsx
+++ part2/2.6 to 2.10/src/App.jsx
@@ -2,10 +2,18 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [newFilter, setNewFilter] = useState("");
+
+  function handleFilterChange(event) {
+    setNewFilter(event.target.value);
+  }
 
   function addPerson(event) {
     event.preventDefault();
@@ -33,6 +41,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with{" "}
+        <input value={newFilter} onChange={handleFilterChange}></input>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameInputChange} />
@@ -45,11 +58,22 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
+      {persons.reduce((filtered, person) => {
+        if (person.name.toLowerCase().includes(newFilter.toLowerCase())) {
+          filtered.push(
+            <p key={person.name}>
+              {person.name} {person.number}
+            </p>
+          );
+        }
+        return filtered;
+      }, [])}
+      {/* <h2>Numbers</h2>
       {persons.map((person) => (
-        <div key={person.name}>
-          {person.name} {person.number}
-        </div>
-      ))}
+
+        <div key={person.name}>{person.name} {person.number}</div>
+      
+      ))} */}
     </div>
   );
 };
