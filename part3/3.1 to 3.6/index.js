278301fa74794a21d134c1d2b15de51d874c9c48
SEF-3070 <josef.weikert@gmail.com>
2025-03-27 16:28:47 +0000

    3.3: Phonebook backend step 3

--- part3/3.1 to 3.6/index.js
+++ part3/3.1 to 3.6/index.js
@@ -34,6 +34,16 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    const person = persons.find(p => p.id === id)
+    
+    if (person) {
+        res.json(person)
+    } else
+    res.status(404).end()
+})
+
 app.get('/info/', (req, res) => {
     const now = new Date(Date.now())
     const nowUTC = now.toUTCString()
